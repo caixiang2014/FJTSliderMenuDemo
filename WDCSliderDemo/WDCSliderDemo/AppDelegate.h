45f533a0f0a8962acada0ecf6b79500bc3bc8050
fnst001 <fnst123>
2015-07-30 19:08:20 +0800

    new slide

---
+++ WDCSliderDemo/WDCSliderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDCSliderDemo
+//
+//  Created by fnst001 on 15-7-30.
+//  Copyright (c) 2015年 FNST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDCSliderDemo/WDCSliderDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WDCSliderDemo
+//
+//  Created by fnst001 on 15-7-30.
+//  Copyright (c) 2015年 FNST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
